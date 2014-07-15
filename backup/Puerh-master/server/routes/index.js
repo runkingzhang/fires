c7d0e5edbf2edf110836f4e1687b83f889c35109
runkezhang <runkingzhang@163.com>
2014-07-15 16:58:25 +0800

    chonggou2

---
+++ backup/Puerh-master/server/routes/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  home: require('./home'),
+  typo: require('./typo'),
+  font: require('./font'),
+  grid: require('./grid'),
+  ui: require('./ui'),
+  js: require('./js'),
+  start: require('./start')
+};
